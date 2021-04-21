b564fd7fa43fb22e285ad087d05d7b212f017a26
grendd <coolvalerchik@gmail.com>
2021-04-21 14:37:36 +0300

    php and sql

--- setup.sh
+++ setup.sh
@@ -15,11 +15,13 @@ echo  "\e[32mDocker build images...\e[0m"
 docker build -t nginx_image ./srcs/nginx/
 docker build -t mysql_image ./srcs/mysql/
 docker build -t wp_image ./srcs/wordpress/
+docker build -t php_image ./srcs/phpMyAdmin/
 
 echo  "\e[32mStart pods and services...\e[0m"
 kubectl apply -f ./srcs/configmap.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/phpMyAdmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
 #kubectl apply -f ./srcs/ingress.yaml
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -8,7 +8,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql' );
